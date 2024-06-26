19a5ff088962685386eac059fc1fa3592fde170b
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-26 10:11:47 +0300

    solves distance travelled in feet

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+  const hqBlock =42;
+
+  return Math.abs(block -hqBlock)
+}
+
+function distanceFromHqInFeet (block){
+  return distanceFromHqInBlocks(block) * 264;
+}
+ 
+ function distanceTravelledInFeet (start, destination){
+  const block = 264;
+  return Math.abs(start-destination) * block;
+ }
+ 
